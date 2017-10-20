6e8ac8eb3b2290d60fd64b955a782735c3e760b5
bah99 <31727552+bah99@users.noreply.github.com>
2017-10-20 09:32:04 +0200

    La classe Lettre et le sacAlettre

---
+++ src/Lettre.java
@@ -0,0 +1,37 @@
+
+public class Lettre {
+	protected String lettre;
+	protected int point;
+	protected int nbFois;
+	
+	
+	//Constructeur 
+	public Lettre(String lettre, int point, int nbFois) {
+		super();
+		this.lettre = lettre;
+		this.point = point;
+		this.nbFois = nbFois;
+	}
+	
+	//getters and setters
+	public String getLettre() {
+		return lettre;
+	}
+	public void setLettre(String lettre) {
+		this.lettre = lettre;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	public int getNbFois() {
+		return nbFois;
+	}
+	public void setNbFois(int nbFois) {
+		this.nbFois = nbFois;
+	}
+	
+
+}
